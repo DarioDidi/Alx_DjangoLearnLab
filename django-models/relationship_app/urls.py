1d5f607efa60eee56d5f904bc8e7aa9119f0ddca
DD-tech <>
2024-11-07 13:28:47 +0300

    added views import with change names to urls.py as separate

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from .views  import list_books, LibraryDetailView
+from .views  import list_books 
+from .views import LibraryDetailView
 urlpatterns = [
     path('listbooks/', list_books, name="list_books"),
     path('displaylibrary/', LibraryDetailView.as_view(), name="display_library"),
