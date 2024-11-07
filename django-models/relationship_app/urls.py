064f7db9557eb18f63a602820d466c5788f3299c
DD-tech <>
2024-11-07 13:31:40 +0300

    too specific, terrible unit tests lol

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from .views import LibraryDetailView
-from .views  import list_books 
+from .views import list_books
 urlpatterns = [
     path('listbooks/', list_books, name="list_books"),
     path('displaylibrary/', LibraryDetailView.as_view(), name="display_library"),
