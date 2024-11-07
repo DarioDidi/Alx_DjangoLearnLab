489eae35696ac67a7a0c24787c11b249052246ff
DD-tech <>
2024-11-07 13:07:46 +0300

    return self.name2

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -9,11 +9,17 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length = 50)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
 
 class Library(models.Model):
     name = models.CharField(max_length = 50)
     books = models.ManyToManyField(Book)
+    def __str__(self):
+        return self.name
 
 class Librarian(models.Model):
     name = models.CharField(max_length = 50)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
