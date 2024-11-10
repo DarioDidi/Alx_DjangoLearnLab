25d380a7af0f5142232e8dc860e29a34c1ccad05
DD-tech <>
2024-11-10 14:08:02 +0300

    added decorator import

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -8,7 +8,8 @@ from django.contrib.auth import views
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 from django.utils.decorators import method_decorator
 # Create your views here.
 
