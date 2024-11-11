6347b449669d6565fa4106cb6727fd78510fca31
DD-tech <>
2024-11-11 14:20:07 +0300

    added customer user manager

--- advanced_features_and_security/relationship_app/models.py
+++ advanced_features_and_security/relationship_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.signals import post_init, post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
 
 # Create your models here.
 
