5b3a01d05b526dd69fbf098a85ee11cb601308a9
DD-tech <>
2024-11-13 00:03:14 +0300

    final security

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -120,7 +120,7 @@ SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 
-
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
