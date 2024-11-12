9856bf586c8ed9ad5065068561fd27615933080f
DD-tech <>
2024-11-12 23:45:25 +0300

    added an example form

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,16 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import permission_required
 from django.core.serializers import serialize
 
 # Create your views here.
 
-
 from .models import Book
+from .forms import ExampleForm
 
 
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create(request):
-    pass
+    if request.method == "POST":
+        form = ExampleForm(request.POST)
+        if form.is_valid:
+            content = form.cleaned_data
+            b = Book.objects.create(
+                name=form.name, publication_year=form.publication_year)
+            b.save()
+            return redirect("")
+    else:
+        form = ExampleForm
 
 
 @permission_required('bookshelf.can_edit', raise_exception=True)
