4fe9a663de14acf189a73e1ad08131d50c911136
DD-tech <>
2024-12-03 16:45:34 +0300

    renamed edit path to update

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('posts/', PostListView.as_view(), name='posts'),
     path('post/new', PostCreateView.as_view(), name='create'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-    path('post/<int:pk>/edit', PostUpdateView.as_view(), name='update'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='update'),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name='delete'),
 ]
+#  ["post/<int:pk>/delete/", "post/<int:pk>/update/", "post/new/"
